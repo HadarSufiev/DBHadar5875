87eee22ddf292ee6aa7eeb99a7df3c605db5e15c
HadarSufiev <38730510+HadarSufiev@users.noreply.github.com>
2025-03-18 20:34:13 +0200

    Update NurserySchoolDropTable.sql

--- Phase1/scripts/NurserySchoolDropTable.sql
+++ Phase1/scripts/NurserySchoolDropTable.sql
@@ -1,8 +1,17 @@
-drop TABLE public.ParentBayby;
-drop TABLE public.Baby;
-drop TABLE public.NanyGroup;
-drop TABLE public.Telephone;
-drop TABLE  public.Parent;
-drop TABLE  public.Nanny;
-drop TABLE public.Person;
-drop TABLE public.Group;
+-- מחיקת טבלאות קישור (כי יש בהן מפתחות זרים)
+DROP TABLE IF EXISTS public.BabyApotropus;
+DROP TABLE IF EXISTS public.NanyVacation;
+DROP TABLE IF EXISTS public.NanyGroup;
+DROP TABLE IF EXISTS public.Enrollment;
+DROP TABLE IF EXISTS public.Telephone;
+
+-- מחיקת ישויות תלויות
+DROP TABLE IF EXISTS public.Baby;
+DROP TABLE IF EXISTS public.Vacation;
+DROP TABLE IF EXISTS public.Apotropus;
+DROP TABLE IF EXISTS public.Nanny;
+
+-- מחיקת ישויות ראשיות
+DROP TABLE IF EXISTS public.Person;
+DROP TABLE IF EXISTS public.BabyGroup;
+
