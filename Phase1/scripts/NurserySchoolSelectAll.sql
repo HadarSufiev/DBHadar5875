0a86f3ad508f630a2be40921a3c59968600d4c15
HadarSufiev <38730510+HadarSufiev@users.noreply.github.com>
2025-03-24 09:36:56 +0200

    Update NurserySchoolSelectAll.sql

--- Phase1/scripts/NurserySchoolSelectAll.sql
+++ Phase1/scripts/NurserySchoolSelectAll.sql
@@ -1,25 +1,25 @@
 -- שליפת כל הנתונים מכל הטבלאות
-SELECT * FROM public.BabyApotropus;
-SELECT * FROM public.NanyVacation;
-SELECT * FROM public.NanyGroup;
-SELECT * FROM public.Enrollment;
-SELECT * FROM public.Telephone;
-SELECT * FROM public.Baby;
-SELECT * FROM public.Vacation;
-SELECT * FROM public.Apotropus;
-SELECT * FROM public.Nanny;
-SELECT * FROM public.Person;
-SELECT * FROM public.BabyGroup;
+SELECT * FROM BabyApotropus;
+SELECT * FROM NanyVacation;
+SELECT * FROM NanyGroup;
+SELECT * FROM Enrollment;
+SELECT * FROM Telephone;
+SELECT * FROM Baby;
+SELECT * FROM Vacation;
+SELECT * FROM Apotropus;
+SELECT * FROM Nanny;
+SELECT * FROM Person;
+SELECT * FROM BabyGroup;
 
 -- ספירת כמות הרשומות בכל טבלה
-SELECT 'BabyApotropus' AS table_name, COUNT(*) FROM public.BabyApotropus;
-SELECT 'NanyVacation' AS table_name, COUNT(*) FROM public.NanyVacation;
-SELECT 'NanyGroup' AS table_name, COUNT(*) FROM public.NanyGroup;
-SELECT 'Enrollment' AS table_name, COUNT(*) FROM public.Enrollment;
-SELECT 'Telephone' AS table_name, COUNT(*) FROM public.Telephone;
-SELECT 'Baby' AS table_name, COUNT(*) FROM public.Baby;
-SELECT 'Vacation' AS table_name, COUNT(*) FROM public.Vacation;
-SELECT 'Apotropus' AS table_name, COUNT(*) FROM public.Apotropus;
-SELECT 'Nanny' AS table_name, COUNT(*) FROM public.Nanny;
-SELECT 'Person' AS table_name, COUNT(*) FROM public.Person;
-SELECT 'BabyGroup' AS table_name, COUNT(*) FROM public.BabyGroup;
+SELECT 'BabyApotropus' AS table_name, COUNT(*) FROM BabyApotropus;
+SELECT 'NanyVacation' AS table_name, COUNT(*) FROM NanyVacation;
+SELECT 'NanyGroup' AS table_name, COUNT(*) FROM NanyGroup;
+SELECT 'Enrollment' AS table_name, COUNT(*) FROM Enrollment;
+SELECT 'Telephone' AS table_name, COUNT(*) FROM .elephone;
+SELECT 'Baby' AS table_name, COUNT(*) FROM Baby;
+SELECT 'Vacation' AS table_name, COUNT(*) FROM Vacation;
+SELECT 'Apotropus' AS table_name, COUNT(*) FROM Apotropus;
+SELECT 'Nanny' AS table_name, COUNT(*) FROM Nanny;
+SELECT 'Person' AS table_name, COUNT(*) FROM Person;
+SELECT 'BabyGroup' AS table_name, COUNT(*) FROM BabyGroup;
